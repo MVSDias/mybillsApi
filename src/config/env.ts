6b0a0f67895356073864243f1dbed8a8860ff4aa
MVSDIAS <marcusdias_00@outlook.com>
2025-08-07 21:39:05 -0300

    env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 // validando as variáveis de ambiente usando zod.
 const envSchema = z.object({
-  PORT: z.string().transform(Number).default("3001"),
+  PORT: z.string().transform(Number),
   DATABASE_URL: z.string().min(5, "DATABASE_URL é obrigatório"),
   NODE_ENV: z.enum(["dev", "test", "prod"], {
     message: "O node_env deve ser dev, teste ou prod",
@@ -21,7 +21,7 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-  console.error("Variáveis de ambiente INVÁLIDAS");
+  console.error("Variáveis de ambiente INVÁLIDAS", _env.error.format());
   process.exit(1); // encerra o processo com erro.
 }
 
